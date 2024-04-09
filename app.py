840cbf351ccda763e32f2abd6b84154791f28de2
chennakesavabollepalli <90134009+chennakesavabollepalli@users.noreply.github.com>
2024-04-09 12:43:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask
+from redis import Redis
+
 app = Flask(__name__)
+redisDB = Redis(host='redis-db', port=6380)
 visitCount = 0
 
 @app.route('/')
 
 def main():
-  global visitCount
-  visitCount+=1
+  #global visitCount
+  #visitCount+=1
+  redisDB.incr('visitorCount')
+  visitCount = str(redisDB.get('visitorCount'),'utf-8')
   return "Hay, loading from Python Codebase!" + str(visitCount)
 if __name__ == "__main__":
   app.run(host='0.0.0.', debug=True)
