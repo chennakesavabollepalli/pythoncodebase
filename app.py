5d82d75233cfcc1767435b2f24465f4c366e94c6
chennakesavabollepalli <90134009+chennakesavabollepalli@users.noreply.github.com>
2024-04-09 12:31:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
+visitCount = 0
 
 @app.route('/')
 
 def main():
-  print ("Hay, loding from Python Codebase")
+  global visitCount
+  visitCount+=1
+  return "Hay, loading from Python Codebase!" + str(visitCount)
 if __name__ == "__main__":
-  app.run(host='0.0.0.')
+  app.run(host='0.0.0.', debug=True)
 
    
